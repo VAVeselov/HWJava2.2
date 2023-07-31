bfec787f1c854c6036f6f7a513a212199113aaaf
Влад Веселов <veselo.vlad9976@gmail.com>
2023-07-31 21:55:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 100;
+        int i = 100;
+        int x = 300;
+        int balance;
+        if (x > 1000)
+        {
+            balance = amount + x + x / i;
+
+    } else {
+            balance = amount + x;
+        }
+        System.out.println(balance);
+
+    }
+}
